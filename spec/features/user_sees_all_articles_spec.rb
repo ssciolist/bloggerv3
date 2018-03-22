f44a8b952c2cba952fe3545e4e4e165e05e097b1
Megan Arellano <ssciolist@gmail.com>
2018-03-22 15:09:05 -0600

    adds new article link

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -12,6 +12,7 @@ describe "user sees all articles" do
       expect(page).to have_content(article_1.body)
       expect(page).to have_link(article_2.title)
       expect(page).to have_content(article_2.body)
+      expect(page).to have_link("Create a New Article")
     end
   end
 end
