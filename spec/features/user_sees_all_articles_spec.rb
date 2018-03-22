6f2791ceee3b22d7dd7d9f838260358b9f0d1adf
Megan Arellano <ssciolist@gmail.com>
2018-03-22 13:53:54 -0600

    Add all articles feature spec

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "user sees all articles" do
+  describe "they visit /articles" do
+    it "displays all articles" do
+      article_1 = Article.create!(title: "Denver Post is dying", body: "Because of greed")
+      article_2 = Article.create!(title: "Journalism is dying", body: "It's failing to adapt")
+
+      visit "/articles"
+
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_1.body)
+      expect(page).to have_content(article_2.title)
+      expect(page).to have_content(article_2.body)
+    end
+  end
+end
